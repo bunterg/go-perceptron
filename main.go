6d7a26138ecb57e4f7249ff6173b053982a9a538
Bernardo Garcia <bmgg358@gmail.com>
2019-03-13 19:59:44 -0400

    added activation function as params, and random train

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"encoding/csv"
+	"fmt"
+	"io"
 	"log"
 	"math"
+	"os"
+	"strconv"
 )
 
 const (
@@ -14,11 +19,66 @@ const (
 )
 
 func main() {
-	log.Println("hola mundo rnn")
-	largestNumber := uint64(math.Pow(2, binaryDimension))
-	idk := 01111111
-	log.Println(byte(largestNumber))
-	log.Println(idk)
+	fileTestInput, err := os.Open("sample/iris_in.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fileTestInput.Close()
+	fileTestOutput, err := os.Open("sample/iris_out.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fileTestOutput.Close()
+	iTest := readerToDataInput(csv.NewReader(fileTestInput))
+	oTest := readerToDataOutput(csv.NewReader(fileTestOutput))
+
+	p := NewPerceptron(4, 0.1, func(res float64) float64 {
+		out := math.Min(res, 2)
+		out = math.Max(res, 0)
+		if out > 0 || out < 2 {
+			out = 1
+		}
+		return out
+	})
+	p.Train(iTest, oTest, 5)
+}
+
+func readerToDataInput(r *csv.Reader) [][]float64 {
+	res := make([][]float64, 150)
+	i := 0
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		res[i] = make([]float64, len(record))
+		for j, r := range record {
+			res[i][j], _ = strconv.ParseFloat(r, 64)
+		}
+		i++
+		fmt.Println(record)
+	}
+	return res
+}
+
+func readerToDataOutput(r *csv.Reader) []float64 {
+	res := make([]float64, 150)
+	i := 0
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		res[i], _ = strconv.ParseFloat(record[0], 64)
+		i++
+	}
+	return res
 }
 
 // ACTIVATION FUNCTIONS
--- perceptron.go
+++ perceptron.go
@@ -1,49 +1,84 @@
 package main
 
-import "errors"
+import (
+	"errors"
+	"log"
+	"math/rand"
+)
 
 // Perceptron network structure
 type Perceptron struct {
-	input   uint
-	weights []float64
-	bias    []float64
+	input       uint
+	weights     []float64
+	learingRate float64
+	activation  func(res float64) float64
 }
 
 // NewPerceptron creates a perceptron network structure
-func NewPerceptron(i uint) *Perceptron {
-	weights := make([]float64, i)
-	bias := make([]float64, i)
+func NewPerceptron(i uint, lR float64, activation func(res float64) float64) *Perceptron {
+	weights := make([]float64, i+1)
+	for i := 0; i < len(weights); i++ {
+		weights[i] = rand.Float64()
+	}
 	return &Perceptron{
-		input:   i,
-		weights: weights,
-		bias:    bias,
+		input:       i,
+		weights:     weights,
+		learingRate: lR,
+		activation:  activation,
 	}
 }
 
 // Train network with given input and result
-func (p *Perceptron) Train(in [][]float64, o []float64) {
+func (p *Perceptron) Train(in [][]float64, o []float64, logs int) error {
+	if len(in) != len(o) {
+		return errors.New("Wrong input/output dimension size")
+	}
+	for _, i := range rand.Perm(len(in)) {
+		res, _ := p.Predict(in[i])
+		// res = activation(res)
+		p.updateWeights(res, o[i])
+		if i%logs == 0 {
+			log.Printf("EPOCH: %d \n", i)
+			log.Printf("Out: %f \n", o[i])
+			log.Printf("RES: %f \n", res)
+			log.Printf("Succes: %t \n", res == o[i])
+			log.Printf("BIAS: %f \n", p.GetBias())
+			log.Println("QUERY:")
+			log.Println(in[i])
+			log.Println("WEIGHTS:")
+			log.Println(p.GetWeights())
+		}
 
+	}
+	return nil
 }
 
-// Exec performs compute
-func (p *Perceptron) Exec(in []float64) (float64, error) {
-	if uint(len(in)) != p.input {
+// Predict performs compute
+func (p *Perceptron) Predict(in []float64) (float64, error) {
+	if len(in) != len(p.weights)-1 {
 		return 0, errors.New("Wrong input dimension size")
 	}
-	return 0, nil
+	var res float64
+	res = p.weights[0]
+	for i := 1; i < len(in)+1; i++ {
+		res += in[i-1] * p.weights[i]
+	}
+	return p.activation(res), nil
+}
+
+// updateWeights update each weight value
+func (p *Perceptron) updateWeights(res float64, o float64) {
+	for i := 0; i < len(p.weights); i++ {
+		p.weights[i] = p.weights[i] + p.learingRate*(o-res)
+	}
 }
 
 // GetWeights returns current Bias
 func (p *Perceptron) GetWeights() []float64 {
-	return p.weights
+	return p.weights[1:]
 }
 
 // GetBias returns current bias
-func (p *Perceptron) GetBias() []float64 {
-	return p.bias
-}
-
-// GetError returns current error
-func (p *Perceptron) GetError() float64 {
-	return 0
+func (p *Perceptron) GetBias() float64 {
+	return p.weights[0]
 }
